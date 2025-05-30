af4376094fec081c02c8f5313e1881be1b143a2a
CHRISTIAN ASMUSSEN <christian@casmuss.com>
2025-05-29 20:37:49 -0400

    Add Pub/Sub topic and subscription creation to ETL script; update Docker image tag in Artifact Registry script

--- 5cinco.bash
+++ 5cinco.bash
@@ -4,3 +4,11 @@ gcloud config list
 gcloud pubsub topics create pipeline-success 
 
 gcloud pubsub topics create pipeline-error
+
+gcloud pubsub subscriptions create pipeline-success \
+  --topic=pipeline-success \
+  --ack-deadline=20
+
+gcloud pubsub subscriptions create pipeline-error \
+  --topic=pipeline-error \
+  --ack-deadline=20
